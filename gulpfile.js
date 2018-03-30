e54998febc5d0c1ea1a75f3718adb81595f49e85
mmoharem <cloud@bnfstore.com>
2018-03-30 11:15:33 +0200

    postcss part1

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,43 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    simplVars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
+
 
 //creat gulp default task
 gulp.task('default', function() {
     console.log("Hooray - you created a Gulp task.");
 }); //Use ($gulp) to run default task.
 
-//new task. 
+
+
+//new task.   //For html
 gulp.task('html', function() {
     console.log("New Gulp task");
 }); //Use ($gulp taskName(html)) to run any task.
 
-//new task. 
+
+
+//new task.   //for css
 gulp.task('styles', function() {
-    console.log("Imagine Sass or PostCSS tasks runing here.");
+    // console.log("Imagine Sass or PostCSS tasks runing here.");
+
+    // Creat gulp.src & pip gulp.dest
+    return gulp.src('./app/assets/css/style.css') //we use return because gulp.src is a syncronas function
+        .pipe(postcss([autoprefixer, simplVars, nested]))
+        .pipe(gulp.dest('./app/temp/styles'));
+
+
 }); //Use ($gulp taskName(html)) to run any task.
 
-//Creat gulp-watch task   
-// gulp.task('watch', function() {
 
-//     // HTML-watch.
-//     watch('./app/index.html', function() {
-//         gulp.start('html');
-//     });
 
-//     watch('./app/assets/styles/**/*.css', function() {
-//         gulp.start('styles');
-//     });
-// });
+
+
+
+//...creat Gulp Watch...
 
 gulp.task('watch', function() {
 
@@ -36,6 +46,7 @@ gulp.task('watch', function() {
         gulp.start('html');
     });
 
+    // CSS-watch.
     watch('./app/assets/css/style.css', function() {
         gulp.start('styles');
     });
